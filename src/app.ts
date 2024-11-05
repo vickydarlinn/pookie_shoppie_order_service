0fc6baeb33e6a132bd6a78c117ade2b5ea523a83
vickydarlinn <uttapalsangwan@gmail.com>
2024-11-05 10:26:11 +0530

    feat:add global error handler and parsers

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,11 @@
 import express, { Request, Response } from "express";
-import { globalErrorHandler } from "./middlewares/globalErrorHandler";
+import { globalErrorHandler } from "./common/middlewares/globalErrorHandler";
+import cookieParser from "cookie-parser";
 
 const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
 
 app.get("/", (req: Request, res: Response) => {
   res.json({ message: "Hello from catalog service!" });
